fae90cac6370632060a8d834ee2765afb43bb1b3
Taggs33 <taggart3317@gmail.com>
2015-09-26 12:06:51 -0500

    finished cool-pose

--- js/app.js
+++ js/app.js
@@ -1,30 +1,60 @@
-$(document).ready(function () {
-	$('.ryu').mouseenter(function() {
+$(document).ready(function() {
+
+$(document).keydown(function(key) {
+		if(key.which == 88) {
+			
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+		}
+		
+	});
+
+$(document).on('keyup',function(event){
+	if (event.which == 88){
+		$('.ryu-cool').hide();
+		$('.ryu-still').show();
+	};
+});				
+
+	$('.ryu').mouseenter(function(){
 		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
+		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	})
 
-	.mouseleave(function() {
+	.mouseleave(function(){
 		$('.ryu-ready').hide();
+		$('.ryu-cool').hide();
+		$('.ryu-throwing').hide();
 		$('.ryu-still').show();
 	})
 
-	.mousedown(function() {
+	.mousedown(function(){
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show().animate(
 			{'left': '1020px'},
 			500,
-			function() {
+			function(){
 				$(this).hide();
 				$(this).css('left', '560px');
-			}
-		);	
+			});	
 	})
 
-	.mouseup(function() {
+	.mouseup(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-		// ryu goes back to his ready position
 	});
+
+
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
